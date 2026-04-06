b43ee85b7d58172a1111b99465c9169c017f08ad
namsoobin <01059152266a@gmail.com>
2026-04-06 09:52:26 +0900

    Fix print_info function and variable names
    Corrected function parameters and fixed variable names.

--- code/5_func.py
+++ code/5_func.py
@@ -1,17 +1,18 @@
 #5_func.py
 
-def print_info(name, phonee, addess, email):
+def print_info(name, phone, addess, email):
     contact_info = f"\t연락처:\t{phone}\n이메일:{email}"
-    return f"이름: \t{name}\n{contact_inffo}\n주소:\ㅅ{address}"
+    return f"이름: \t{name}\n{contact_inffo}\n주소:\t{address}"
 
-def print_info(name, phonee, addess, email):
-    contact_info = f"휴대폰:\t{phone}\n{email}"
-    print(f"이름: \t{name}\n{contact_inffo}\n\주소:\t{address}")
+def print_info(name, phonee, addess, email=""):
+    contact_info = f"휴대폰:\t{phone}\n이메일:\t{email}"
+    print(f"이름:\t{name}\n{contact_inffo}\n\주소:\t{address}")
     
 print_info("Aaron","010-5555-5555","전주")
 person = return_info(
     email="hi@ut.ac.kr", phone="010-1111-1111",
     address="교통대학교", name = "aaron"
     )
+
 print()
 print(person)
