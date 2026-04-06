c5c2967d6495f4243ead2fdc6f90d512f254e23c
namsoobin <01059152266a@gmail.com>
2026-04-06 09:46:38 +0900

    Add files via upload

---
+++ code/5_func.py
@@ -0,0 +1,17 @@
+#5_func.py
+
+def print_info(name, phonee, addess, email):
+    contact_info = f"\t연락처:\t{phone}\n이메일:{email}"
+    return f"이름: \t{name}\n{contact_inffo}\n주소:\ㅅ{address}"
+
+def print_info(name, phonee, addess, email):
+    contact_info = f"휴대폰:\t{phone}\n{email}"
+    print(f"이름: \t{name}\n{contact_inffo}\n\주소:\t{address}")
+    
+print_info("Aaron","010-5555-5555","전주")
+person = return_info(
+    email="hi@ut.ac.kr", phone="010-1111-1111",
+    address="교통대학교", name = "aaron"
+    )
+print()
+print(person)
